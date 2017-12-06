4268c397f777c8b299040495658ff4cbb9f8eed2
ruiqin.shen <ruiiqn.shen@ecaray.com>
2017-12-06 19:40:31 +0800

    升级版本号

--- app/src/main/java/com/ruiqin/rxjava/module/rxjava/RxJavaDemo0Activity.java
+++ app/src/main/java/com/ruiqin/rxjava/module/rxjava/RxJavaDemo0Activity.java
@@ -8,7 +8,12 @@ import com.ruiqin.rxjava.base.BaseActivity;
 import com.ruiqin.rxjava.util.ToastUtils;
 
 import butterknife.OnClick;
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
+import io.reactivex.FlowableOnSubscribe;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Consumer;
 
@@ -35,6 +40,8 @@ public class RxJavaDemo0Activity extends BaseActivity {
             case R.id.btn_hello_lambda:
                 helloRxJavaLambda();
                 break;
+            default:
+                break;
         }
     }
 
@@ -56,6 +63,19 @@ public class RxJavaDemo0Activity extends BaseActivity {
                 throwable.printStackTrace();
             }
         });
+
+        Flowable.create((FlowableOnSubscribe<Integer>) e -> {
+
+        }, BackpressureStrategy.BUFFER);
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+
+            }
+        });
+
     }
 
     /**
